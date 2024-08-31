1ad5fd415bacbffc5c2f84aba6dd1746b9def751
vinicius <vini_laraujo@hotmail.com>
2024-08-31 18:04:26 -0300

    Fix filtered event query

--- src/main/java/com/eventostec/api/repositories/EventRepository.java
+++ src/main/java/com/eventostec/api/repositories/EventRepository.java
@@ -22,7 +22,7 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
             "WHERE (:title = '' OR e.title LIKE %:title%) " +
             "AND (:city = '' OR a.city LIKE %:city%)" +
             "AND (:uf = '' OR a.uf LIKE %:uf%) " +
-            "AND (a.startDate >= :startDate AND e.endDate <= :endDate)")
+            "AND (e.date >= :startDate AND e.date <= :endDate)")
     public Page<Event> findFilteredEvents( @Param("title") String title,
                                           @Param("city") String city,
                                           @Param("uf") String uf,
